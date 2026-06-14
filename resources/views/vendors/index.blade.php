aa7ed21df23c732ef3fc7472bac6a01884622361
Reza <rezaalie70@gmail.com>
2026-06-14 18:33:33 +0330

    Fix data-stack page stuck on loading logo in stack page

--- resources/views/vendors/index.blade.php
+++ resources/views/vendors/index.blade.php
@@ -12,7 +12,7 @@
     <!-- Style css -->
     <link class="main-css" href="../../../css/style.css" rel="stylesheet">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
 
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
